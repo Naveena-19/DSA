3e230f42315980e906bea4a4d648c8012a76d5f0
naveena-19 <naveenakumari1902@example.com>
2024-04-05 11:25:28 +0530

    even/odd

---
+++ src/Bitwise/Even_ood.java
@@ -0,0 +1,20 @@
+package Bitwise;
+
+public class Even_ood {
+
+        public static void main(String[] args)
+        {
+            int n=10;
+            if((10&1)==1)
+//                1&anyNumber =anyNumber
+//                 0&anyNumber=0
+
+            {
+                System.out.println("odd");
+            }
+            else {
+                System.out.println("even");
+            }
+        }
+
+}
