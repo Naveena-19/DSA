a3ade81666a0a6c55d0a36420be76d56d2e77eab
naveena-19 <naveenakumari1902@example.com>
2024-04-08 21:26:24 +0530

    find whether given number is power of 2

--- src/Bitwise/Power_2.java
+++ src/Bitwise/Power_2.java
@@ -4,19 +4,33 @@ public class Power_2 {
     public static void main(String[] args)
     {
         int n=256;
-        while(n>0)
-        {
-            int t=(n&1);
-            n=n>>1;
-            if(t==1&&n==0){
+        while(n>0) {
+            int t = (n & 1);
+            n = n >> 1;
+            if (t == 1 && n == 0) {
                 System.out.println("it is power of 2");
-                            }
-            if(t==1&&n!=0)
-            {
+            }
+            if (t == 1 && n != 0) {
                 System.out.println("it is not power of 2");
                 break;
 
             }
+        }
+
+
+
+
+
+//        METHOD 2
+        if((n&(n-1))==0)
+        {
+//           eg: here n=1000
+//                  n-1=0111
+            System.out.println("it is power of 2");
+        }
+        else {
+            System.out.println("it is power of 2");
+
         }
     }
 }
