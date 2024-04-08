899e91483953f0ce8a5c5711d85419be3be16f0d
naveena-19 <naveenakumari1902@example.com>
2024-04-08 21:07:27 +0530

    sum of nth row in pascal triangle

---
+++ src/Bitwise/Binominal.java
@@ -0,0 +1,11 @@
+package Bitwise;
+
+public class Binominal {
+    public static void main(String[] args) {
+//        for pascal trainagle
+//        sum of nth row is nC0 +nC1+---+nCn-1+nCn equals pow(2,n-1)
+
+        System.out.println(1+5+10+10+5+1);// 6th row
+        System.out.println(Math.pow(2,5));
+    }
+}
