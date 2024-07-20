3367348bd6a3cb1f15da06813432a689cde6b028
Naveena_19 <naveenakumari1902@gmail.com>
2024-07-20 15:54:54 +0530

    permutations

---
+++ src/Recursion_subseque/Permuatation.java
@@ -0,0 +1,32 @@
+package Recursion_subseque;
+
+import java.util.ArrayList;
+
+public class Permuatation
+{
+    public static void main(String[] args)
+    {
+
+        ArrayList<String> permuList = new ArrayList<>();
+        permu("", "abc", permuList);
+        System.out.println(permuList);
+    }
+
+
+    private static void permu(String s1, String s2, ArrayList<String> permuList) {
+        if (s2.isEmpty()) {
+            permuList.add(s1);
+            return;
+        }
+        char ch= s2.charAt(0);
+        for(int i=0;i<=s1.length();i++)
+        {
+            String f=s1.substring(0,i);
+            String s=s1.substring(i,s1.length());
+            permu(f+ch+s,s2.substring(1),permuList);
+        }
+
+
+    }
+
+}
